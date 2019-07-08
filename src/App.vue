9cc30b2be4216b79db5ab884563d2354c229b5c2
牛亮 <nll@itcast.cn>
2019-07-08 15:04:03 +0800

    删除示意代码

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
   <div id="app">
-    <h3>我就试试</h3>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- 3. 使用组件 -->
-    <hero-list></hero-list>
+   
   </div>
 </template>
 
 <script>
-// 1. 导入组件
-import HeroList from './components/HeroList.vue'
 
 export default {
   name: 'app',
   // 2. 注册局部组件
   components: {
-    HeroList
   }
 }
 </script>
