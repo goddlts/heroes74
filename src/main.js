bb361bfd979ad83b40c94da80ad50fe60b985036
牛亮 <nll@itcast.cn>
2019-07-08 15:11:51 +0800

    在App.vue中实现首页

--- src/main.js
+++ src/main.js
@@ -1,22 +1,15 @@
-// 程序的入口
-// 加载vue模块 CommonJS的模块化语法
-// const Vue = require('vue')
-
 // ES6中的模块化的语法，导入模块
 import Vue from 'vue'
 
 // 根组件
 import App from './App.vue'
 
-// 引入外部样式
-// import './xxx/index.css'
-
 // 提示当前处在开发模式, false 不提示
 Vue.config.productionTip = false
 
-// new Vue({
-//   el: '#app'
-// })
+// 导入样式
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
 
 new Vue({
   render: createElement => createElement(App),
